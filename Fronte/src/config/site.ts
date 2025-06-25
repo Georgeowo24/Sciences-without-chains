d06277bb79949198fd7bac6c30cfb10f939b17d4
Jorge Torres Romero <jtorresromero30@gmail.com>
2025-06-25 16:45:25 -0600

    Integrate HeroUI and build layout with Header and Footer

---
+++ Fronte/src/config/site.ts
@@ -0,0 +1,57 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Vite + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Categories",
+      href: "/docs",
+    },
+    {
+      label: "About Us",
+      href: "/pricing",
+    },
+    {
+      label: "Help",
+      href: "/blog",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help",
+      href: "/help",
+    },
+    {
+      label: "Log In",
+      href: "/LogIn",
+    },
+    {
+      label: "Sign Up",
+      href: "/SignUp",
+    },
+    {
+      label: "Log Out",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/frontio-ai/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
